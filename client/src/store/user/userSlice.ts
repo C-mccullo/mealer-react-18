2ee76d043ebad41e80f22f685c96253a45714908
Colin McCullough <14228256+C-mccullo@users.noreply.github.com>
2023-05-09 01:38:03 -0400

    removing unused import

--- client/src/store/user/userSlice.ts
+++ client/src/store/user/userSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction, isRejectedWithValue } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { User } from '../../types/index.types';
 
 interface LoggedInState {
